bb593dd7b78490a78e4358229b90433c5f5816cd
rachealkuranchie <kuranchieracheal@gmail.com>
2025-01-30 14:14:43 +0000

    fixes

--- vibes/vibes/urls.py
+++ vibes/vibes/urls.py
@@ -19,9 +19,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
+
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('api/auth/', include('vibez_api.users.urls')),
     path('api/', include('vibez_api.posts.urls'))
 ]
